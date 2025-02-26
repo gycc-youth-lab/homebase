13d9c70c82e6c45612c8da4023dadd54b0f7c46d
bchen-gycc <bchen@gyccyouthlab.org>
2025-02-26 16:20:14 +0900

    photo gallery component largely completed

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Card, Modal, ModalContent, Image as HeroUiImage } from "@heroui/react";
+import { Card, Modal, ModalContent, Image as HeroUiImage, Pagination } from "@heroui/react";
 import Image from 'next/image';
 
 interface Photo {
@@ -12,11 +12,25 @@ type Props = {
     bucketName: string;
 };
 
+const IMAGES_PER_PAGE = 16;
+
 const PhotoGallery: React.FC<Props> = ({ bucketName }) => {
     const [photos, setPhotos] = useState<Photo[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+    const [totalImages, setCount] = useState<number>(0);
     const hasFetched = useRef(false);
+
+    // for pagination
+    const totalPages = Math.ceil(totalImages / IMAGES_PER_PAGE);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [currentImages, setCurrentImages] = useState<Photo[]>([]);
+
+    const controlDisplay = (pgNum: number) => {
+        const startIndex = (pgNum - 1) * IMAGES_PER_PAGE;
+        const displayedImages = photos.slice(startIndex, startIndex + IMAGES_PER_PAGE);
+        setCurrentImages(displayedImages);
+    }
 
     // for modal
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
@@ -33,8 +47,9 @@ const PhotoGallery: React.FC<Props> = ({ bucketName }) => {
                 setError('Failed to fetch photos');
             }
             const { images, count } = await response.json();
-
+            setCount(count);
             setPhotos(images);
+            setCurrentImages(images.slice(0, IMAGES_PER_PAGE));
         } catch (err) {
             console.error('Error fetching photos:', err);
             setError('Failed to fetch photos');
@@ -56,25 +71,28 @@ const PhotoGallery: React.FC<Props> = ({ bucketName }) => {
     }
 
     return (
-        <div className='grid grid-cols-4 gap-4 p-4'>
-            {photos.map(photo => (
-                <Card key={photo.uuid} isPressable
-                    onPress={() => setSelectedImage(photo.url)}
-                    className="relative w-full aspect-square overflow-hidden">
-                    <Image fill src={photo.url} alt={photo.filePath}
-                        className="w-full h-full object-cover" />
-                </Card>
-            ))}
-
+        <>
+            {loading && <div>Loading...</div>}
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
+                {currentImages.map(photo => (
+                    <Card key={photo.uuid} isPressable
+                        onPress={() => setSelectedImage(photo.url)}
+                        className="relative w-full aspect-square overflow-hidden">
+                        <Image fill src={photo.url} alt={photo.filePath} // TODO: figure out how to use HeroUiImage here
+                            className="w-full h-full object-cover" />
+                    </Card>
+                ))}
+            </div>
+            {/* Pagination */}
+            {!loading && <Pagination showControls initialPage={currentPage} total={totalPages} onChange={(pageNum) => { setCurrentPage(pageNum); controlDisplay(pageNum); }} />}
             {/* Lightbox Modal */}
             <Modal isOpen={!!selectedImage} onOpenChange={() => setSelectedImage(null)} size="lg">
                 <ModalContent>
                     {selectedImage && <HeroUiImage src={selectedImage} alt="Enlarged image" width="100%" height="auto" />}
                 </ModalContent>
             </Modal>
-            {loading && <div>Loading...</div>}
-        </div>
+        </>
     );
 };
 
-export default PhotoGallery
+export default PhotoGallery;
