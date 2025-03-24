f8a13c0af4d0fd07b52f01be8cf3bd080ccdda38
bchen-gycc <bchen@gyccyouthlab.org>
2025-03-24 16:10:05 -0700

    made refinement to NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,33 +1,38 @@
 'use client'
 
-import React from 'react';
+import { useState } from 'react';
 
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link } from "@heroui/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Button } from "@heroui/react";
 
 import Image from "next/image";
+import Link from 'next/link';
 
-export default function App() {
+const menuItems = [
+  { label: "GYCC", href: "/" },
+  { label: "Activities", href: "#" },
+  { label: "Future Generation", href: "#" },
+  { label: "Our Voice", href: "#" },
+  { label: "Join Us", href: "#" },
+];
 
+const Logo = () => (
+  <Image src="/GYCC_logo.png" alt="GYCC Logo" width={200} height={40} className='w-[200px]' />
+)
 
-  const menuItems = [
-    { label: "GYCC", href: "#" },
-    { label: "Activities", href: "#" },
-    { label: "Future Generation", href: "#" },
-    { label: "Our Voice", href: "#" },
-    { label: "Join Us", href: "#" },
-  ];
+export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
 
   return (
-    <Navbar  shouldHideOnScroll className="flex items-center justify-start bg-white">
-      <NavbarBrand className="sm:flex-grow-0 basis-auto">
-        <Image src="/logo.png" alt="GYCC Logo" width={200} height={40} className="w-[200px] h-[40px]" />
-        <NavbarContent className="sm:hidden">
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarBrand className="sm:flex-grow-0 basis-auto">
+          <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className='sm:hidden' />
+          <Logo />
+        </NavbarBrand>
       </NavbarContent>
-      <NavbarMenuToggle className='sm:hidden text-black'
-    
-        />
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-8">
+
+      <NavbarContent className="hidden sm:flex gap-8" justify='center'>
         {menuItems.map((item, index) => (
           <NavbarItem key={index}>
             <Link
@@ -39,15 +44,23 @@ export default function App() {
           </NavbarItem>
         ))}
       </NavbarContent>
-
-
+      {/* {Not implementing log in feature until further notice} */}
+      {/* <NavbarContent justify='end'>
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent> */}
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={index}>
             <Link
               className="w-full font-bold text-black uppercase tracking-tighter"
               href={item.href}
-              size="lg"
             >
               {item.label}
             </Link>
