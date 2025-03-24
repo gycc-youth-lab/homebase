239cdccf045c9e77efe6afafb3382c551b3f5f5a
mrazaf24 <mrazaf24@gyccyouthlab.org>
2025-03-24 21:56:54 +0900

    updated NavBar responsive

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,31 +1,60 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@heroui/react";
+'use client'
+
+import React from 'react';
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link } from "@heroui/react";
 
 import Image from "next/image";
 
 export default function App() {
+
+
+  const menuItems = [
+    { label: "GYCC", href: "#" },
+    { label: "Activities", href: "#" },
+    { label: "Future Generation", href: "#" },
+    { label: "Our Voice", href: "#" },
+    { label: "Join Us", href: "#" },
+  ];
+
   return (
-      <Navbar shouldHideOnScroll className="flex items-center justify-start bg-white">
-        <NavbarBrand className="flex-grow-0 basis-auto">
-          <Image src="/logo.png" alt="GYCC Logo" width={200} height={40} className="w-[200px] h-[40px]"/>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-8">
-          <NavbarItem>
-            <Link className="font-bold text-black uppercase tracking-tighter" href="#">GYCC</Link>
+    <Navbar  shouldHideOnScroll className="flex items-center justify-start bg-white">
+      <NavbarBrand className="sm:flex-grow-0 basis-auto">
+        <Image src="/logo.png" alt="GYCC Logo" width={200} height={40} className="w-[200px] h-[40px]" />
+        <NavbarContent className="sm:hidden">
+      </NavbarContent>
+      <NavbarMenuToggle className='sm:hidden text-black'
+    
+        />
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-8">
+        {menuItems.map((item, index) => (
+          <NavbarItem key={index}>
+            <Link
+              className="font-bold text-black uppercase tracking-tighter"
+              href={item.href}
+            >
+              {item.label}
+            </Link>
           </NavbarItem>
-          <NavbarItem>
-            <Link className="font-bold text-black uppercase tracking-tighter" href="#">Activities</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link className="font-bold text-black uppercase tracking-tighter" href="#">Future Generation</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link className="font-bold text-black uppercase tracking-tighter" href="#">Our Voice</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link className="font-bold text-black uppercase tracking-tighter" href="#">Join Us</Link>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
+        ))}
+      </NavbarContent>
+
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={index}>
+            <Link
+              className="w-full font-bold text-black uppercase tracking-tighter"
+              href={item.href}
+              size="lg"
+            >
+              {item.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
 
